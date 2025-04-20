a11baf706a19dada92b0ec0a9f7c01a66443e037
kyoshitou <sx2273@nyu.edu>
2025-04-20 16:37:25 -0400

    developing usb

--- Software/src/PAM_Module.c
+++ Software/src/PAM_Module.c
@@ -9,9 +9,31 @@
 
 #include <syslog.h>
 
+#include <libusb-1.0/libusb.h>
+
+
 #include "ZKP_verify.h" // Include your ZKP verification logic
 
 #define ZKP_ROUNDS 3 // Number of challenge-response rounds
+
+#define ENDPOINT_ADDRESS_OUT 0x01
+#define ENDPOINT_ADDRESS_IN 0x81
+
+/*
+USB Protocol:
+
+0x01 - Request Commit
+0x02 - Challenge 0
+0x03 - Challenge 1
+0x04 - End Of protocol: SUCC
+0x05 - End Of protocol: FAIL
+*/
+
+#define USB_REQ_COMMIT "1"
+#define USB_CHA_0 "2"
+#define USB_CHA_1 "3"
+#define USB_END "4"
+
 
 #define USERFILE "/etc/security/users"
 
@@ -20,6 +42,12 @@ static int get_user_public_key(const char *username, char *y_str, size_t max_len
 static void get_prover_commit(char *commit);
 static void challenge_prover(bool challenge);
 static void get_prover_response(char *response);
+
+static int init_USB_key(libusb_context *ctx, libusb_device_handle *dev_handle);
+static int release_USB_key(libusb_context *ctx, libusb_device_handle *dev_handle);
+
+libusb_context *ctx;
+libusb_device_handle *dev_handle;
 
 /*
 Called when no user using ZKP proof was found. Takes in a username and receives the public key,
@@ -137,22 +165,103 @@ static int get_user_public_key(const char *username, char *y_str, size_t max_len
 // Callbacks for ZKP communication
 static void get_prover_commit(char *commit)
 {
-    printf("Enter commitment: ");
-    fflush(stdout);
-    fgets(commit, 1024, stdin);
-    commit[strcspn(commit, "\n")] = 0; // Remove newline
+    int r;
+    int actual_length;
+
+    r = libusb_bulk_transfer(dev_handle,
+        ENDPOINT_ADDRESS_OUT,
+        USB_REQ_COMMIT,
+        sizeof(USB_REQ_COMMIT),
+        &actual_length,
+        1000
+    );
+
+    r = libusb_bulk_transfer(dev_handle,
+        ENDPOINT_ADDRESS_IN,
+        commit,
+        1024,
+        &actual_length,
+        1000
+    );
 }
 
 static void challenge_prover(bool challenge)
 {
     printf("Challenge: %d\n", challenge);
+    unsigned char data_out[2];
+    int actual_length = 2;
+
+    if (challenge == true) {
+        data_out[0] = '1';
+        data_out[1] = '\0';
+    }
+    else {
+        data_out[0] = '0';
+        data_out[1] = '\0';
+    }
+
+    int r;
+    // Send data to the device
+    r = libusb_bulk_transfer(dev_handle,
+                            ENDPOINT_ADDRESS_OUT,
+                            data_out,
+                            sizeof(data_out),
+                            &actual_length,
+                            1000); // timeout in ms
     fflush(stdout);
 }
 
 static void get_prover_response(char *response)
 {
-    printf("Enter response: ");
-    fflush(stdout);
-    fgets(response, 1024, stdin);
-    response[strcspn(response, "\n")] = 0; // Remove newline
+    int r;
+    int actual_length;
+
+    r = libusb_bulk_transfer(dev_handle,
+        ENDPOINT_ADDRESS_IN,
+        response,
+        1024,
+        &actual_length,
+        1000);
 }
+
+
+static int init_USB_key(libusb_context *ctx, libusb_device_handle *dev_handle){
+    int r;
+
+    // Initialize the libusb library
+    r = libusb_init(&ctx);
+    // Optional: Set debug level
+    libusb_set_option(ctx, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_INFO);
+
+    // Open the device using Vendor ID (VID) and Product ID (PID)
+    dev_handle = libusb_open_device_with_vid_pid(ctx, 0x1234, 0x5678);
+    if (!dev_handle) {
+        fprintf(stderr, "Could not open USB device\n");
+        return -1;
+    }
+
+    r = libusb_claim_interface(dev_handle, 0);
+
+    if (r < 0) {
+        fprintf(stderr, "Failed to claim interface\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int release_USB_key(libusb_context *ctx, libusb_device_handle *dev_handle) {
+    int r;
+    int actual_length;
+    // Send data to the device
+    r = libusb_bulk_transfer(dev_handle,
+                            ENDPOINT_ADDRESS_OUT,
+                            USB_END,
+                            sizeof(USB_END),
+                            &actual_length,
+                            1000);
+    libusb_release_interface(dev_handle, 0);
+    libusb_close(dev_handle);
+    libusb_exit(ctx);
+    return 0;
+}   
